158e7680fec0ce62fc9ab49c66dbc940cd01530f
88yhtserof <88yhtserofg@gmail.com>
2025-03-06 20:26:40 +0900

    [FEAT] CoinFlowColor: 프로젝트에서 사용하는 색 객체 정의 #1

---
+++ CoinFlow/CoinFlow/Domain/UseCase/CoinFlowColor.swift
@@ -0,0 +1,23 @@
+//
+//  CoinFlowColor.swift
+//  CoinFlow
+//
+//  Created by 임윤휘 on 3/6/25.
+//
+
+import UIKit
+
+enum CoinFlowColor {
+    
+    static let title: UIColor = .coinflowDarkNavy
+    
+    static let subtitle: UIColor = .coinflowGray
+    
+    static let rise: UIColor = .coinflowRed
+    
+    static let fall: UIColor = .coinflowBlue
+    
+    static let background: UIColor = .coinflowWhite
+    
+    static let subBackground: UIColor = .coinflowLightGray
+}
