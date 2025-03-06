3169d59517b6a0e1e8db6d62408d090f87fa1a3a
88yhtserof <88yhtserofg@gmail.com>
2025-03-06 18:07:24 +0900

    프로젝트 생성 및 설정

---
+++ CoinFlow/CoinFlow/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CoinFlow
+//
+//  Created by 임윤휘 on 3/6/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
