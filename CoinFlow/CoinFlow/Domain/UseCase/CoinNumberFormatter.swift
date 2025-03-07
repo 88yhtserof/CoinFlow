9cc1daebc4a8bee625cfd2f402c85e95279fd9d8
88yhtserof <88yhtserofg@gmail.com>
2025-03-07 20:58:21 +0900

    [FEAT] CoinNumberFormatter 구현 #1

---
+++ CoinFlow/CoinFlow/Domain/UseCase/CoinNumberFormatter.swift
@@ -0,0 +1,78 @@
+//
+//  CoinNumberFormatter.swift
+//  CoinFlow
+//
+//  Created by 임윤휘 on 3/7/25.
+//
+
+import Foundation
+
+enum CoinNumberFormatter {
+    case tradePrice(number: Double)
+    case changeRate(number: Double)
+    case changePrice(number: Double)
+    case accTradePrice(number: Double)
+    
+    var string: String? {
+        switch self {
+        case .tradePrice(let number):
+            return CoinNumberFormatter.decimal(by: .tradePrice(number as NSNumber))
+        case .changeRate(let number):
+            return CoinNumberFormatter.percent(number, point: 2)
+        case .changePrice(let number):
+            return CoinNumberFormatter.decimal(by: .changePrice(number as NSNumber))
+        case .accTradePrice(let number):
+            return CoinNumberFormatter.truncToMilions(number)
+        }
+    }
+}
+
+//MARK: - Format
+private extension CoinNumberFormatter {
+    enum FormatterType {
+        case tradePrice(NSNumber)
+        case changePrice(NSNumber)
+    }
+    
+    static let tradePriceFormatter: NumberFormatter = {
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .decimal
+        formatter.maximumFractionDigits = 2
+        formatter.minimumFractionDigits = 1
+        return formatter
+    }()
+    
+    static let changePriceFormatter: NumberFormatter = {
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .decimal
+        formatter.maximumFractionDigits = 2
+        formatter.minimumFractionDigits = 2
+        return formatter
+    }()
+}
+
+//MARK: - Configuration
+private extension CoinNumberFormatter {
+    
+    static func truncToMilions(_ number: Double) -> String {
+        if number > 1000000 {
+            let value = Int(number) / 1000000
+            return String(format: "%@백만", value.formatted())
+        } else {
+            return number.formatted()
+        }
+    }
+    
+    static func percent(_ number: Double, point: Int) -> String {
+        return String(format: "%.\(point)f%%", number)
+    }
+    
+    static func decimal(by formatter: FormatterType) -> String? {
+        switch formatter {
+        case .tradePrice(let number):
+            return CoinNumberFormatter.tradePriceFormatter.string(from: number)
+        case .changePrice(let number):
+            return CoinNumberFormatter.changePriceFormatter.string(from: number)
+        }
+    }
+}
