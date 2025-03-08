a41486f718dfa8766288e469254afd5214bc571d
88yhtserof <88yhtserofg@gmail.com>
2025-03-08 22:51:07 +0900

    [FEAT] CoinTrending: 검색바 구현 및 배치 #3

--- CoinFlow/CoinFlow/Presentation/CoinTrending/CoinTrendingViewController.swift
+++ CoinFlow/CoinFlow/Presentation/CoinTrending/CoinTrendingViewController.swift
@@ -9,6 +9,8 @@ import UIKit
 
 final class CoinTrendingViewController: UIViewController {
     
+    private let searchBar = SearchBar()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -24,13 +26,17 @@ private extension CoinTrendingViewController {
     func configureView() {
         view.backgroundColor = CoinFlowColor.background
         navigationItem.leftBarButtonItem = TitleBarButtonItem(title: "가상자산 / 심볼 검색")
+        
     }
     
     func configureHierarchy() {
-        
+        view.addSubviews([searchBar])
     }
     
     func configureConstraints() {
-        
+        searchBar.snp.makeConstraints { make in
+            make.horizontalEdges.equalTo(view.safeAreaLayoutGuide).inset(16)
+            make.top.equalTo(view.safeAreaLayoutGuide).inset(8)
+        }
     }
 }
