a8d1433d14491c7e7a4eb3e76da2ff96d0b495d7
88yhtserof <88yhtserofg@gmail.com>
2025-03-09 01:14:48 +0900

    [FEAT] CoinTrending: 인기 검색어 cell UI 구성 #3

--- CoinFlow/CoinFlow/Presentation/CoinTrending/CoinTrendingViewController.swift
+++ CoinFlow/CoinFlow/Presentation/CoinTrending/CoinTrendingViewController.swift
@@ -7,9 +7,16 @@
 
 import UIKit
 
+import SnapKit
+import RxSwift
+import RxCocoa
+
 final class CoinTrendingViewController: UIViewController {
     
     private let searchBar = SearchBar()
+    private lazy var collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout())
+    
+    private let disposeBag = DisposeBag()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -17,6 +24,16 @@ final class CoinTrendingViewController: UIViewController {
         configureHierarchy()
         configureConstraints()
         configureView()
+        bind()
+    }
+    
+    private func bind() {
+        
+        Observable.just(["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14"])
+            .bind(to: collectionView.rx.items(cellIdentifier: TrendingSearchKeywordCollectionViewCell.identifier,
+                                              cellType: TrendingSearchKeywordCollectionViewCell.self)) { item, element, cell in
+                cell.configure(with: element)
+            }.disposed(by: disposeBag)
     }
 }
 
@@ -27,16 +44,75 @@ private extension CoinTrendingViewController {
         view.backgroundColor = CoinFlowColor.background
         navigationItem.leftBarButtonItem = TitleBarButtonItem(title: "가상자산 / 심볼 검색")
         
+        collectionView.register(TrendingSearchKeywordCollectionViewCell.self, forCellWithReuseIdentifier: TrendingSearchKeywordCollectionViewCell.identifier)
+        collectionView.isScrollEnabled = false
     }
     
     func configureHierarchy() {
-        view.addSubviews([searchBar])
+        view.addSubviews([searchBar, collectionView])
     }
     
     func configureConstraints() {
+        
         searchBar.snp.makeConstraints { make in
             make.horizontalEdges.equalTo(view.safeAreaLayoutGuide).inset(16)
             make.top.equalTo(view.safeAreaLayoutGuide).inset(8)
         }
+        
+        collectionView.snp.makeConstraints { make in
+            make.horizontalEdges.equalTo(view.safeAreaLayoutGuide)
+            make.top.equalTo(searchBar.snp.bottom)
+            make.bottom.equalTo(view.safeAreaLayoutGuide)
+        }
+    }
+}
+
+//MARK: - CollectionView Layout
+private extension CoinTrendingViewController {
+    func layout() -> UICollectionViewLayout {
+        let configuration = UICollectionViewCompositionalLayoutConfiguration()
+        configuration.interSectionSpacing = 10
+        return UICollectionViewCompositionalLayout(sectionProvider: sectionProviderHandler, configuration: configuration)
+    }
+    
+    func sectionProviderHandler(sectionIndex: Int, layoutEnvironment: NSCollectionLayoutEnvironment) -> NSCollectionLayoutSection {
+        guard let section = Section(rawValue: sectionIndex) else {
+            fatalError("Could not find section")
+        }
+        
+        switch section {
+        case .searchKeyword:
+            return sectionForSearchKeyword()
+        }
+    }
+    
+    func sectionForSearchKeyword() -> NSCollectionLayoutSection {
+        let height = view.frame.height
+        
+        let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .fractionalHeight(1.0 / 7.0))
+        let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(0.45), heightDimension: .absolute(height / 4.0))
+        
+        let item = NSCollectionLayoutItem(layoutSize: itemSize)
+        let group = NSCollectionLayoutGroup.vertical(layoutSize: groupSize, subitems: [item])
+        group.interItemSpacing = .flexible(4)
+        
+        let section = NSCollectionLayoutSection(group: group)
+        section.orthogonalScrollingBehavior = .continuous
+        section.interGroupSpacing = 10
+        section.contentInsets = NSDirectionalEdgeInsets(top: 10, leading: 10, bottom: -10, trailing: -10)
+        
+        return section
+    }
+    
+    func sectionForNFT() {
+        
+    }
+}
+
+//MARK: - CollectionView DataSource
+private extension CoinTrendingViewController {
+    
+    enum Section: Int {
+        case searchKeyword
     }
 }
---
+++ CoinFlow/CoinFlow/Presentation/CoinTrending/TrendingSearchKeywordCollectionViewCell.swift
@@ -0,0 +1,117 @@
+//
+//  TrendingSearchKeywordCollectionViewCell.swift
+//  CoinFlow
+//
+//  Created by 임윤휘 on 3/8/25.
+//
+
+import UIKit
+
+import SnapKit
+
+final class TrendingSearchKeywordCollectionViewCell: UICollectionViewCell {
+    
+    static let identifier = String(describing: TrendingSearchKeywordCollectionViewCell.self)
+    
+    private let rankLabel = UILabel()
+    private let iconImageView = UIImageView()
+    private let nameLabel = UILabel()
+    private let symbolLabel = UILabel()
+    private lazy var nameStackView = UIStackView(arrangedSubviews: [nameLabel, symbolLabel])
+    private let changeImageView = UIImageView()
+    private let changeLabel = UILabel()
+    private lazy var changeStackView = UIStackView(arrangedSubviews: [changeImageView, changeLabel])
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        configureHierarchy()
+        configureConstraints()
+        configureView()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func configure(with value: String) {
+        rankLabel.text = value
+    }
+}
+
+//MARK: - Configuration
+private extension TrendingSearchKeywordCollectionViewCell {
+    
+    func configureView() {
+        rankLabel.text = "1"
+        rankLabel.textColor = CoinFlowColor.title
+        rankLabel.font = .systemFont(ofSize: 12, weight: .regular)
+        rankLabel.textAlignment = .center
+        
+        iconImageView.image = UIImage(systemName: "photo.circle")
+        iconImageView.tintColor = CoinFlowColor.subtitle
+        
+        nameLabel.text = "COIN"
+        nameLabel.textColor = CoinFlowColor.title
+        nameLabel.font = .systemFont(ofSize: 12, weight: .bold)
+        
+        symbolLabel.text = "Flow"
+        symbolLabel.textColor = CoinFlowColor.subtitle
+        symbolLabel.font = .systemFont(ofSize: 9, weight: .regular)
+        
+        nameStackView.axis = .vertical
+        nameStackView.spacing = 0
+        nameStackView.alignment = .leading
+        
+        changeImageView.image = UIImage(systemName: "arrowtriangle.up.fill")
+        changeImageView.tintColor = CoinFlowColor.title
+        
+        changeLabel.text = "1.23%"
+        changeLabel.textColor = CoinFlowColor.title
+        changeLabel.font = .systemFont(ofSize: 9, weight: .bold)
+        changeLabel.textAlignment = .right
+        
+        changeStackView.axis = .horizontal
+        changeStackView.spacing = 2
+        changeStackView.alignment = .center
+    }
+    
+    func configureHierarchy() {
+        contentView.addSubviews([rankLabel, iconImageView, nameStackView, changeStackView])
+    }
+    
+    func configureConstraints() {
+        
+        let inset: CGFloat = 14
+        let offset: CGFloat = 4
+        
+        
+        rankLabel.snp.makeConstraints { make in
+            make.width.equalTo(20)
+            make.centerY.equalToSuperview()
+            make.leading.equalToSuperview().inset(inset)
+        }
+        
+        iconImageView.snp.makeConstraints { make in
+            make.size.equalTo(26)
+            make.centerY.equalToSuperview()
+            make.leading.equalTo(rankLabel.snp.trailing).offset(offset)
+        }
+        
+        nameStackView.snp.makeConstraints { make in
+            make.centerY.equalToSuperview()
+            make.leading.equalTo(iconImageView.snp.trailing).offset(offset)
+        }
+        
+        changeImageView.snp.makeConstraints { make in
+            make.size.equalTo(9)
+        }
+        
+        changeStackView.snp.makeConstraints { make in
+            make.centerY.equalToSuperview()
+            make.leading.greaterThanOrEqualTo(nameLabel.snp.trailing)
+            make.trailing.equalToSuperview().inset(inset)
+        }
+        
+    }
+}
