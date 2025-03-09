a2c29dcb4f859dc72c125720ae966a47dcbf12c9
88yhtserof <88yhtserofg@gmail.com>
2025-03-09 11:56:50 +0900

    [FEAT] CoinTrending: 인기 NFT #3

--- CoinFlow/CoinFlow/Presentation/CoinTrending/CoinTrendingViewController.swift
+++ CoinFlow/CoinFlow/Presentation/CoinTrending/CoinTrendingViewController.swift
@@ -16,6 +16,8 @@ final class CoinTrendingViewController: UIViewController {
     private let searchBar = SearchBar()
     private lazy var collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout())
     
+    private var dataSource: DataSource!
+    
     private let disposeBag = DisposeBag()
     
     override func viewDidLoad() {
@@ -24,16 +26,17 @@ final class CoinTrendingViewController: UIViewController {
         configureHierarchy()
         configureConstraints()
         configureView()
+        configureDataSource()
         bind()
     }
     
     private func bind() {
         
-        Observable.just(["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14"])
-            .bind(to: collectionView.rx.items(cellIdentifier: TrendingSearchKeywordCollectionViewCell.identifier,
-                                              cellType: TrendingSearchKeywordCollectionViewCell.self)) { item, element, cell in
-                cell.configure(with: element)
-            }.disposed(by: disposeBag)
+//        Observable.just(["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14"])
+//            .bind(to: collectionView.rx.items(cellIdentifier: TrendingSearchKeywordCollectionViewCell.identifier,
+//                                              cellType: TrendingSearchKeywordCollectionViewCell.self)) { item, element, cell in
+//                cell.configure(with: element)
+//            }.disposed(by: disposeBag)
     }
 }
 
@@ -83,6 +86,8 @@ private extension CoinTrendingViewController {
         switch section {
         case .searchKeyword:
             return sectionForSearchKeyword()
+        case .nft:
+            return sectionForNFT()
         }
     }
     
@@ -104,15 +109,72 @@ private extension CoinTrendingViewController {
         return section
     }
     
-    func sectionForNFT() {
+    func sectionForNFT() -> NSCollectionLayoutSection {
+        let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .fractionalHeight(1))
+        let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0 / 4.0), heightDimension: .absolute(110))
         
+        let item = NSCollectionLayoutItem(layoutSize: itemSize)
+        let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitems: [item])
+        
+        let section = NSCollectionLayoutSection(group: group)
+        section.orthogonalScrollingBehavior = .continuous
+        section.interGroupSpacing = 10
+        section.contentInsets = NSDirectionalEdgeInsets(top: 10, leading: 10, bottom: 10, trailing: 10)
+        
+        return section
     }
 }
 
 //MARK: - CollectionView DataSource
 private extension CoinTrendingViewController {
     
-    enum Section: Int {
+    enum Section: Int, CaseIterable {
         case searchKeyword
+        case nft
+    }
+    
+    enum Item: Hashable {
+        case searchKeyword(Int)
+        case nft(Int)
+    }
+    
+    typealias DataSource = UICollectionViewDiffableDataSource<Section, Item>
+    typealias Snapshot = NSDiffableDataSourceSnapshot<Section, Item>
+    
+    func configureDataSource() {
+        let trendingSearchKeywordCellRegistration = UICollectionView.CellRegistration(handler: trendingSearchKeywordCellRegistrationHandler)
+        let trendingNFTCellRegistration = UICollectionView.CellRegistration(handler: trendingNFTCellRegistrationHandler)
+        
+        dataSource = DataSource(collectionView: collectionView,
+                                cellProvider: { collectionView, indexPath, item in
+            switch item {
+            case .searchKeyword(let value):
+                return collectionView.dequeueConfiguredReusableCell(using: trendingSearchKeywordCellRegistration, for: indexPath, item: value)
+            case .nft(let value):
+                return collectionView.dequeueConfiguredReusableCell(using: trendingNFTCellRegistration, for: indexPath, item: value)
+            }
+        })
+        
+        createSnapshot([])
+        collectionView.dataSource = dataSource
+    }
+    
+    func trendingSearchKeywordCellRegistrationHandler(cell: TrendingSearchKeywordCollectionViewCell, indexPath: IndexPath, item: Int) {
+        
+    }
+    
+    func trendingNFTCellRegistrationHandler(cell: TrendingNFTCollectionViewCell, indexPath: IndexPath, item: Int) {
+        
+    }
+    
+    func createSnapshot(_ items: [Item]) {
+        let list1 = Array(1...14).map{ Item.searchKeyword($0) }
+        let list2 = Array(1...7).map{ Item.nft($0) }
+        
+        var snapshot = Snapshot()
+        snapshot.appendSections(Section.allCases)
+        snapshot.appendItems(list1, toSection: .searchKeyword)
+        snapshot.appendItems(list2, toSection: .nft)
+        dataSource.applySnapshotUsingReloadData(snapshot)
     }
 }
---
+++ CoinFlow/CoinFlow/Presentation/CoinTrending/TrendingNFTCollectionViewCell.swift
@@ -0,0 +1,101 @@
+//
+//  TrendingNFTCollectionViewCell.swift
+//  CoinFlow
+//
+//  Created by 임윤휘 on 3/9/25.
+//
+
+import UIKit
+
+import SnapKit
+
+final class TrendingNFTCollectionViewCell: UICollectionViewCell {
+    
+    static let identifier = String(describing: TrendingNFTCollectionViewCell.self)
+    
+    private let iconImageView = UIImageView()
+    private let nameLabel = UILabel()
+    private let floorPrice = UILabel()
+    private lazy var nameStackView = UIStackView(arrangedSubviews: [nameLabel, floorPrice])
+    private let changeImageView = UIImageView()
+    private let changeLabel = UILabel()
+    private lazy var changeStackView = UIStackView(arrangedSubviews: [changeImageView, changeLabel])
+    private lazy var outerStackView = UIStackView(arrangedSubviews: [iconImageView, nameStackView, changeStackView])
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        configureHierarchy()
+        configureConstraints()
+        configureView()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func configure(with value: String) {
+        
+    }
+}
+
+//MARK: - Configuration
+private extension TrendingNFTCollectionViewCell {
+    
+    func configureView() {
+        
+        iconImageView.image = UIImage(systemName: "photo.circle")
+        iconImageView.tintColor = CoinFlowColor.subtitle
+        
+        nameLabel.text = "COIN"
+        nameLabel.textColor = CoinFlowColor.title
+        nameLabel.font = .systemFont(ofSize: 9, weight: .bold)
+        
+        floorPrice.text = "Flow"
+        floorPrice.textColor = CoinFlowColor.subtitle
+        floorPrice.font = .systemFont(ofSize: 9, weight: .regular)
+        
+        nameStackView.axis = .vertical
+        nameStackView.spacing = 0
+        nameStackView.alignment = .center
+        
+        changeImageView.image = UIImage(systemName: "arrowtriangle.up.fill")
+        changeImageView.tintColor = CoinFlowColor.title
+        
+        changeLabel.text = "1.23%"
+        changeLabel.textColor = CoinFlowColor.title
+        changeLabel.font = .systemFont(ofSize: 9, weight: .bold)
+        changeLabel.textAlignment = .right
+        
+        changeStackView.axis = .horizontal
+        changeStackView.spacing = 2
+        changeStackView.distribution = .equalSpacing
+        
+        outerStackView.axis = .vertical
+        outerStackView.spacing = 5
+        outerStackView.alignment = .center
+    }
+    
+    func configureHierarchy() {
+        contentView.addSubviews([outerStackView])
+    }
+    
+    func configureConstraints() {
+        iconImageView.snp.makeConstraints { make in
+            make.height.equalTo(72)
+            make.width.equalTo(iconImageView.snp.height)
+        }
+        
+        changeImageView.snp.makeConstraints { make in
+            make.width.equalTo(9)
+        }
+        
+        changeStackView.snp.makeConstraints { make in
+            make.height.equalTo(9)
+        }
+        
+        outerStackView.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
+    }
+}
