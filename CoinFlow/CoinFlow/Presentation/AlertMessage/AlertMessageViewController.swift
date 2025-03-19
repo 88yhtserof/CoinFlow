0e5372c3619794b8148f0e04a7d47bc88030932d
88yhtserof <88yhtserofg@gmail.com>
2025-03-19 17:35:25 +0900

    [FEAT] AlertMessage: 화면 UI 구현 #9

---
+++ CoinFlow/CoinFlow/Presentation/AlertMessage/AlertMessageViewController.swift
@@ -0,0 +1,102 @@
+//
+//  AlertMessageViewController.swift
+//  CoinFlow
+//
+//  Created by 임윤휘 on 3/19/25.
+//
+
+import UIKit
+
+import SnapKit
+
+final class AlertMessageViewController: UIViewController {
+    
+    private let deemBackgroundView = UIView()
+    private let alertBackgroundView = UIView()
+    private let titleLabel = UILabel()
+    private let messageLabel = UILabel()
+    private lazy var alertStackView = UIStackView(arrangedSubviews: [titleLabel, messageLabel])
+    private let button = UIButton()
+    
+    var message: String? {
+        get { messageLabel.text }
+        set { messageLabel.text = newValue }
+    }
+    
+    var buttonTitle: String? {
+        get { button.title(for: .normal) }
+        set { button.setTitle(newValue, for: .normal) }
+    }
+    
+    var buttonHandler: (() -> Void)?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        configureHierarchy()
+        configureConstraints()
+        configureView()
+    }
+    
+    @objc private func didButtonTapped() {
+        buttonHandler?()
+    }
+}
+
+//MARK: - Configuration
+private extension AlertMessageViewController {
+    
+    func configureView() {
+        
+        deemBackgroundView.backgroundColor = .black
+        deemBackgroundView.alpha = 0.2
+        
+        alertBackgroundView.backgroundColor = CoinFlowColor.background
+        
+        titleLabel.text = "안내"
+        titleLabel.font = .systemFont(ofSize: 14, weight: .bold)
+        titleLabel.textColor = CoinFlowColor.title
+        titleLabel.textAlignment = .center
+        
+        messageLabel.font = .systemFont(ofSize: 14, weight: .regular)
+        messageLabel.textColor = CoinFlowColor.title
+        messageLabel.textAlignment = .center
+        messageLabel.numberOfLines = 0
+        
+        alertStackView.axis = .vertical
+        alertStackView.spacing = 16
+        alertStackView.distribution = .fill
+        alertStackView.alignment = .fill
+        
+        button.setTitleColor(CoinFlowColor.title, for: .normal)
+        button.addTarget(self, action: #selector(didButtonTapped), for: .touchUpInside)
+    }
+    
+    func configureHierarchy() {
+        view.addSubviews([deemBackgroundView, alertBackgroundView])
+        alertBackgroundView.addSubviews([alertStackView, button])
+    }
+    
+    func configureConstraints() {
+        
+        deemBackgroundView.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
+        
+        alertBackgroundView.snp.makeConstraints { make in
+            make.centerY.equalToSuperview()
+            make.horizontalEdges.equalToSuperview().inset(32)
+        }
+        
+        alertStackView.snp.makeConstraints { make in
+            make.top.horizontalEdges.equalToSuperview().inset(16)
+        }
+        
+        button.snp.makeConstraints { make in
+            make.top.equalTo(alertStackView.snp.bottom).offset(26)
+            make.bottom.equalToSuperview().inset(16)
+            make.centerX.equalToSuperview()
+            make.width.equalTo(200)
+        }
+    }
+}
